68d7be509befff9cfa8415fd1a35d8de62fc5d6c
safwan <safwan.nghs2@gmail.com.com>
2024-09-09 11:10:24 +0600

    discount.js

---
+++ discount.js
@@ -0,0 +1,21 @@
+function discvountedPreice(quantity){
+ if(quantity<=100){
+   const total= quantity * 100;
+   return total;
+
+ }
+else if(quantity<=200){
+  const total= quantity*90;
+  return total;
+
+
+}
+else {
+  const total = quantity* 70;
+  return total;
+
+
+}
+}
+const total = discvountedPreice(1000);
+console.log(total);
